9259c537ec21c7579ac2c38837a367e38eab8643
Felipe Haddad <fehaddad@gmail.com>
2025-07-02 15:03:14 -0300

    ex7

---
+++ Lista07-String/exercicio7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 200
+
+void conta_m_m(char frase[]) {
+    int contMa=0, contMi=0;
+    for(int i=0;frase[i]!='\0';i++){
+        if(frase[i]>='a' && frase[i]<='z'){
+            contMi++;
+        }
+        else if(frase[i]>='A' && frase[i]<='Z'){
+            contMa++;
+        }
+    }
+    printf("%d Maiusculos e %d Minusculos\n", contMa, contMi);
+}
+
+int main() {
+    char frase[MAX];
+    printf("Digite uma frase: ");
+    fgets(frase, MAX, stdin);
+
+    conta_m_m(frase);
+
+    return 0;
+}
