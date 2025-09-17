487065cbc2657e09c102088a7804acc7706d8912
Felipe Haddad <fehaddad@gmail.com>
2025-09-17 11:10:37 -0300

    exer2

--- Lista10-AlocacaoDinamica/exercicio2.c
+++ Lista10-AlocacaoDinamica/exercicio2.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cadastrar(int **IDs){
-    *IDs = (float*) malloc(sizeof(float)*7);
-
-    for(int i=0; i<7; i++){
-    printf("Digite o valor do %dº dia: ",i+1);
-    scanf("%f", &(*IDs)[i]);
-    getchar();
-   }
-}
-
-void gastoTotal(int IDs[]){
+void gastoTotal(float IDs[]){
     int gasto=0;
     for(int i=0; i<7; i++){
         gasto+=IDs[i];
@@ -19,7 +9,7 @@ void gastoTotal(int IDs[]){
     printf("Gasto total: %d\n", gasto);
 }
 
-void maiorGasto(int IDs[]){
+void maiorGasto(float IDs[]){
     int maior=0;
     for(int i=0; i<7; i++){
         if(IDs[i]>maior){
@@ -31,9 +21,15 @@ void maiorGasto(int IDs[]){
 }
 
 int main() {
-    int *IDs = NULL;
+    float *IDs = NULL;
+    IDs = (float*) malloc(sizeof(float)*7);
 
-    cadastrar(&IDs);
+    for(int i=0; i<7; i++){
+    printf("Digite o valor do %dº dia: ",i+1);
+    scanf("%f", &IDs[i]);
+    getchar();
+   }
+
     gastoTotal(IDs);
     maiorGasto(IDs);
 
